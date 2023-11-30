5cf18381d82a50341d468192b76974411a78657b
chanif <chanifuddin@live.com>
2023-11-30 21:13:38 +0700

    .

---
+++ build_push_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build Docker image
+docker build -t ghcr.io/chanif/shipping-service:latest .
+
+# Log in to GitHub Container Registry
+echo $GH_PACKAGES_TOKEN | docker login ghcr.io -u chanif --password-stdin
+
+# Push Docker image to GitHub Container Registry
+docker push ghcr.io/chanif/shipping-service:latest
